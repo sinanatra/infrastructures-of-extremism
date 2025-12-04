44c2a4d14ad4daa1bd1499ace34c38738a460e25
Giacomo Nanni <gn.nanni@gmail.com>
2025-12-04 14:42:32 +0100

    feat larger set

--- app/scripts/precompute-layout.js
+++ app/scripts/precompute-layout.js
@@ -187,8 +187,8 @@ const loadData = async () => {
 
 const computeLayout = ({ posts, links, groups }) => {
   const TAU = Math.PI * 2;
-  const width = 4000;
-  const height = 4000;
+  const width = 5000;
+  const height = 5000;
   const cx = width / 2;
   const cy = height / 2;
   const innerRadius = 3;
