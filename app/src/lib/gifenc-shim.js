095e3bf83abdd0efee33e9dc5c728d47dfe34d41
Giacomo Nanni <gn.nanni@gmail.com>
2025-12-05 17:31:32 +0100

    fix vercel build

--- app/src/lib/gifenc-shim.js
+++ app/src/lib/gifenc-shim.js
@@ -1,27 +1,37 @@
-import * as gifencNs from 'gifenc';
+class GIFEncoder {
+  constructor() {
+    throw new Error("GIF export is not supported in this build.");
+  }
+}
 
-const gifenc = gifencNs.default ?? gifencNs;
+const applyPalette = () => {};
+const nearestColor = () => null;
+const nearestColorIndex = () => -1;
+const nearestColorIndexWithDistance = () => ({ index: -1, distance: Infinity });
+const prequantize = (input) => input;
+const quantize = (input) => input;
+const snapColorsToPalette = (input) => input;
 
-const {
-	GIFEncoder,
-	applyPalette,
-	nearestColor,
-	nearestColorIndex,
-	nearestColorIndexWithDistance,
-	prequantize,
-	quantize,
-	snapColorsToPalette
-} = gifenc;
+const gifenc = {
+  GIFEncoder,
+  applyPalette,
+  nearestColor,
+  nearestColorIndex,
+  nearestColorIndexWithDistance,
+  prequantize,
+  quantize,
+  snapColorsToPalette,
+};
 
 export {
-	GIFEncoder,
-	applyPalette,
-	nearestColor,
-	nearestColorIndex,
-	nearestColorIndexWithDistance,
-	prequantize,
-	quantize,
-	snapColorsToPalette
+  GIFEncoder,
+  applyPalette,
+  nearestColor,
+  nearestColorIndex,
+  nearestColorIndexWithDistance,
+  prequantize,
+  quantize,
+  snapColorsToPalette,
 };
 
 export default gifenc;
