3e6214db96eb2b6dd8d42b7e12f73c059d448dd7
Giacomo Nanni <gn.nanni@gmail.com>
2025-12-06 18:06:15 +0100

    chore updated text

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -69,12 +69,13 @@
         <p class="mb-2">
           Right-wing youth organisations have gained greater visibility on
           European streets. During demonstrations, banners often advertise
-          Telegram channels as gateways into tight-knit political communities.
-          For example, at a rally held in Berlin on 29 November 2025 against
-          so-called <em>“criminal foreigners”</em>, participants promoted
-          channels used for youth recruitment and for coordinating activities at
-          the local level. But who is behind these messages? What are they
-          talking about? How are they spread? And who do they reach?
+          Telegram channels as gateways to extremist communities. For example,
+          at a rally held in Berlin on 29 November 2025 against so-called <em
+            >“criminal foreigners”</em
+          >, participants promoted channels used for youth recruitment and for
+          coordinating activities at the local level. But who is behind these
+          messages and what do they say? Moreover, how are they disseminated and
+          who do they reach?
         </p>
 
         <figure class="flex flex-col items-start mt-10 mb-6 gap-2 self-start">
@@ -94,13 +95,14 @@
           <em>Alternative für Deutschland</em> (AfD), a far-right political
           party with seats in parliament and a strongly anti-immigration stance.
           Its former youth wing,
-          <em>Junge Alternative</em>, was recently designated as an extremist
+          <em>Junge Alternative</em>, was recently labelled as an extremist
           organisation and effectively banned from public life. In its place, a
           new network has emerged:
           <em>Generation Deutschland</em>. This group is seeking to take over
           the task of youth mobilisation, using platforms like Telegram even
-          more intensively. In some cases, groups that originally operated under
-          the <em>Junge Alternative</em> name simply rebranded as
+          more intensively. Oddly enough, in a few cases, groups that originally
+          operated under the name <em>Junge Alternative</em> simply changed
+          their username to
           <em>Generation Deutschland</em> and continued their activities as before.
         </p>
 
@@ -111,19 +113,21 @@
             class="w-[220px] block grayscale hover:grayscale-0"
           />
           <figcaption class="text-sm max-w-80 text-gray-600 pb-8 text-right">
-            Telegram profile <em>@JungeAlternativeLSA</em> renamed as
-            <em>Generation Deutschland</em>.
+            The Telegram profile <em>@JungeAlternativeLSA</em> has been renamed
+            <em>Generation Deutschland LSA</em>, although the handle remains the
+            former.
           </figcaption>
         </figure>
 
         <p class="mb-2">
-          Telegram is not only a place for these groups to gather, but also a
-          tool that keeps them connected. Forwarded messages make it easy to
-          jump from one group to another. Messages often cross regional and
-          national borders, linking what might appear to be isolated initiatives
-          into a wider shared media environment. What looks like a local protest
-          may actually be part of a much larger network spanning countries such
-          as Italy, France, Austria, the Netherlands and Poland.
+          Telegram is not only a place where these groups gather, but also a
+          tool that keeps them in constant contact. Forwarded messages make it
+          easy to switch between groups, especially when they are at risk of
+          being banned. Threads often cross regional and national boundaries,
+          connecting what might seem like isolated initiatives into a larger
+          shared media environment. What looks like a local protest may actually
+          be part of a much wider network spanning countries such as Italy,
+          France, Austria, the Netherlands and Poland.
         </p>
 
         <figure class="flex flex-col items-start mt-10 mb-6 gap-2 self-start">
@@ -141,14 +145,14 @@
         </figure>
 
         <p class="mb-2">
-          This investigation begins with the Berlin protest and follows the
-          connections visible on Telegram. Mentions, forwards, and related
-          messages are collected via automated scraping and transformed into a
-          map. This allows us to see how individual groups grow, which channels
-          serve as key hubs, and which connect different national scenes. While
-          this does not provide a complete picture of the movement, it does
-          provide a way to trace some of the infrastructure behind the
-          circulation of today's fascist ideologies.
+          This investigation begins with the protest in Berlin and follows their
+          network visible on Telegram. Mentions, forwards, and related messages
+          are collected via automated scraping and transformed into a map. This
+          allows us to see how individual groups grow, which channels serve as
+          key hubs, and which connect different national scenes. While this does
+          not provide a complete picture of the movement, it does provide a way
+          to trace some of the infrastructure behind the circulation of today's
+          fascist ideologies.
         </p>
       </div>
     </article>
@@ -158,8 +162,12 @@
     class="custom-shadow flex justify-center relative z-10 w-full px-5 pt-10 bg-black"
   >
     <section id="datasets" class="max-w-8xl overflow-auto mx-auto pb-10">
-      <h2 class="text-2xl p-0 m-0 mb-2">Open a map</h2>
+      <h2 class="text-2xl p-0 m-0">Open a map</h2>
 
+      <p class="text-sm max-w-80 text-gray-600 pb-8">
+        Several groups have been examined. Click on each one to explore its
+        network.
+      </p>
       {#if datasets.length}
         <div
           class="flex gap-4 pb-2 overflow-x-auto selection:overflow-visible selection:flex-wrap selection:justify-center"
