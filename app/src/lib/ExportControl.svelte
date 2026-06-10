289d93fce7a45d1822bd42a5ae4a3e64dba8705d
Giacomo Nanni <gn.nanni@gmail.com>
2026-06-10 15:15:56 +0200

    off component

--- app/src/lib/ExportControl.svelte
+++ app/src/lib/ExportControl.svelte
@@ -11,10 +11,10 @@
     dispatch("export");
   };
 </script>
-
+<!-- 
 <button
   class="px-3 py-2 rounded border border-white/20 bg-black/60 text-xs hover:bg-[rgba(255,255,255,0.08)] transition disabled:opacity-60"
   on:click={handleClick}
 >
   export image
-</button>
+</button> -->
